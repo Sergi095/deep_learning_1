c344d70734fc16f9a8598f6c6824937e98b274ca
Sergi095 <sergio.gutierrezmaury@gmail.com>
2022-12-05 14:37:45 +0100

    utils.py

--- assignment3/part1/utils.py
+++ assignment3/part1/utils.py
@@ -35,8 +35,8 @@ def sample_reparameterize(mean, std):
     #######################
     # PUT YOUR CODE HERE  #
     #######################
-    z = None
-    raise NotImplementedError
+    z = torch.randn_like(mean) * std + mean
+    # raise NotImplementedError
     #######################
     # END OF YOUR CODE    #
     #######################
@@ -58,8 +58,8 @@ def KLD(mean, log_std):
     #######################
     # PUT YOUR CODE HERE  #
     #######################
-    KLD = None
-    raise NotImplementedError
+    KLD = torch.sum(0.5 * (log_std.exp() ** 2 + mean ** 2 - 1 - log_std), dim=-1)
+    # raise NotImplementedError
     #######################
     # END OF YOUR CODE    #
     #######################
@@ -78,8 +78,8 @@ def elbo_to_bpd(elbo, img_shape):
     #######################
     # PUT YOUR CODE HERE  #
     #######################
-    bpd = None
-    raise NotImplementedError
+    bpd = elbo / (np.log(2) * np.prod(img_shape[2:]))
+    # raise NotImplementedError
     #######################
     # END OF YOUR CODE    #
     #######################
@@ -110,11 +110,15 @@ def visualize_manifold(decoder, grid_size=20):
     #######################
     # PUT YOUR CODE HERE  #
     #######################
-    img_grid = None
-    raise NotImplementedError
+    z_dim = decoder.z_dim
+    percentiles = torch.linspace(0.5 / grid_size, (grid_size - 0.5) / grid_size, grid_size)
+    z_values = torch.distributions.Normal(0, 1).icdf(percentiles)
+    z1, z2 = torch.meshgrid([z_values] * z_dim)
+    z = torch.stack([z1, z2], dim=-1).reshape(-1, z_dim)
+    imgs = decoder(z).reshape(grid_size, grid_size, *decoder.img_shape)
+    img_grid = make_grid(imgs, nrow=grid_size)
+    # raise NotImplementedError
     #######################
     # END OF YOUR CODE    #
     #######################
-
     return img_grid
-
